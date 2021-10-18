8652f91dbf0342a5285ae09eb97ddde6495853c4
VepeP <57757745+VepeP@users.noreply.github.com>
2021-10-18 10:53:51 +0200

    feat: create student model

--- src/main/java/com/codecool/hogwartshouses/model/StudentModel.java
+++ src/main/java/com/codecool/hogwartshouses/model/StudentModel.java
@@ -1,12 +1,66 @@
 package com.codecool.hogwartshouses.model;
 
+import com.codecool.hogwartshouses.entity.Room;
+import com.codecool.hogwartshouses.entity.Student;
+import com.codecool.hogwartshouses.entity.types.HouseType;
+import com.codecool.hogwartshouses.entity.types.PetType;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @Builder
 public class StudentModel {
+    private long id;
+    private String name;
+    private Room roomId;
+    private PetType petType;
+    private HouseType houseType;
 
+    public StudentModel(Student student) {
+        this.id = student.getId();
+        this.name = student.getName();
+        this.roomId = student.getRoomId();
+        this.petType = student.getPetType();
+        this.houseType = student.getHouseType();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Room getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Room roomId) {
+        this.roomId = roomId;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public HouseType getHouseType() {
+        return houseType;
+    }
+
+    public void setHouseType(HouseType houseType) {
+        this.houseType = houseType;
+    }
 }
