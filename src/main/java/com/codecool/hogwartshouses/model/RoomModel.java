f81883eb07727b43199010c5bad99f80081da4e5
VepeP <57757745+VepeP@users.noreply.github.com>
2021-10-18 10:53:36 +0200

    feat: create room model

--- src/main/java/com/codecool/hogwartshouses/model/RoomModel.java
+++ src/main/java/com/codecool/hogwartshouses/model/RoomModel.java
@@ -1,12 +1,33 @@
 package com.codecool.hogwartshouses.model;
 
+import com.codecool.hogwartshouses.entity.Room;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @Builder
 public class RoomModel {
+    private long id;
+    private boolean needsRenovation;
 
+    public RoomModel(Room room) {
+        this.id = room.getId();
+        this.needsRenovation = room.isNeedsRenovation();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isNeedsRenovation() {
+        return needsRenovation;
+    }
+
+    public void setNeedsRenovation(boolean needsRenovation) {
+        this.needsRenovation = needsRenovation;
+    }
 }
