5ecbe77d169b952a6886bcb840a96a606cfcffe4
VepeP <57757745+VepeP@users.noreply.github.com>
2021-10-18 11:15:58 +0200

    feat: add sample data's to database

---
+++ src/main/resources/data/db_sample.sql
@@ -0,0 +1,17 @@
+insert into room values (1, false);
+insert into room values (2, false);
+insert into room values (3, false);
+insert into room values (4, false);
+insert into room values (5, true);
+insert into room values (6, false);
+insert into room values (7, false);
+insert into room values (8, true);
+insert into room values (9, false);
+insert into room values (10, true);
+
+insert into student values (1, date(current_date), 1, 'Tiffany Ray', 4, 1);
+insert into student values (2, date(current_date), 2, 'Lena Keller', 1, 2);
+insert into student values (3, date(current_date), 3, 'Michael Oxlong', 4, 3);
+insert into student values (4, date(current_date), 1, 'Evie Wood', 3, 4);
+insert into student values (5, date(current_date), 4, 'Rex Middleton', 2, 5);
+insert into student values (6, date(current_date), 2, 'Lawrence Nunez', 4, 6);
