fd6e39d3e15f129fad40a6629df9184b38392734
VepeP <57757745+VepeP@users.noreply.github.com>
2021-10-18 14:10:07 +0200

    feat: create StudentRepository

---
+++ src/main/java/com/codecool/hogwartshouses/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.codecool.hogwartshouses.repository;
+
+import com.codecool.hogwartshouses.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Student findById(long id);
+}
